3ea05cf388cd2a869fed6bd4c6bc2d8d48ba7377
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-21 09:15:32 -0800

    C nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char word[9] = "_putchar";
+	int number;
+
+	for (number = 0; number < 9; number++)
+		_putchar(word[i];
+	_putchar('\n')
+	retun (0);
