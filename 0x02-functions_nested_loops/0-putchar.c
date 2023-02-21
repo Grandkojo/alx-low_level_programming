1f179d9cb1cb33a80c8c49a508a5b0060963d6f1
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-21 10:20:43 -0800

    C nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  *
  * Return: always 0 (Success)
  */
-void _putchar(void);
 int main(void)
 {
 	char word[9] = "_putchar";
 	int number;
 
 	for (number = 0; number < 9; number++)
-		_putchar(word[i];
+		_putchar(word[number]);
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 # ifndef _putchar
 # define _putchar
 
-void _putchar();
+void _putchar (void);
 
 # endif
