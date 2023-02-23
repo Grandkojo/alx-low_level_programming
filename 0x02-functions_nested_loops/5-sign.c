ee0e2e0c4f8ce3ab16aaac647b80acb751995600
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-22 19:55:34 -0800

    C programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		value = 1;
+		num = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		value = 0;
+		num = 0;
 		_putchar('0');
 	}
 	else if (n < 0)
 	{
-		value = -1;
+		num = -1;
 		_putchar('-');
 	}
-	return (value);
+	return (num);
 }
