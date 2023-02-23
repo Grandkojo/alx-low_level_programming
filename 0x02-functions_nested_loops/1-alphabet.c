5d2506ebb454d73dc7644ed05bde50e137eb509b
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-22 17:56:22 -0800

    c programming' '

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+# include "main.h"
+# include <stdio.h>
+# include <stdlib>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+	return (0);
+}
