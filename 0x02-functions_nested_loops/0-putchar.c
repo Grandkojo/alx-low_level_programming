07254610373d541abdacc676f81f279eda9ac387
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-22 16:51:57 -0800

    C programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,7 @@
  * main - Entry point
  *
  * Return: always 0 (Success)
+ *
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-# ifndef _putchar
-# define _putchar
-
-void _putchar (void);
+# ifndef PUTCHAR_H_
+# define PUTCHAR_H_
+int _putchar(char c)
+{
+	return (0);
+}
 
 # endif
