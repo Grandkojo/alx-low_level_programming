a0231e4a32f20dc52985a8a9ce1bc02cf87af827
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 06:47:55 -0800

    C nested

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,14 +10,15 @@ int _isdigit(int c)
 {
 	char num;
 
-	int ret;
+	int ret = 0;
 
 	for (num = '0'; num <= '9'; num++)
 	{
 		if (c == num)
+		{
 			ret = 1;
-		else
-			ret = 0;
+			break;
+		}
 	}
 	return (ret);
 }
