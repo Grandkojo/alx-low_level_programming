c1588848c888a157824cff28f1a4bb0a92434d2c
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 08:08:34 -0800

    C nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * mul - Check dscription
+ * Descriptio: Multiplies two integers
+ * @a: input character
+ * @b: input character
+ * Return: Product of the two numbers
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = (a * b);
+	return (product);
+}
