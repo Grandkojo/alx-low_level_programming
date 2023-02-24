3ce35075248ac0b1f674c85fd8cbfb5153b95033
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 06:32:47 -0800

    C nested

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ * _isdigit - Check description
+ * @c: input character
+ * Description: Checks for a digit (0 through 9)
+ * Return: 1 if c is digit or otehrwise
+ */
+int _isdigit(int c)
+{
+	char num;
+	int ret;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (c == num)
+			ret = 1;
+		else
+			ret = 0;
+	}
+	return (num);
+}
