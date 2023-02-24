5003d67a5b68484f6777eaa2207d60d046d88676
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 05:58:49 -0800

    C nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/**
+ * _isupper - Chack code
+ * @c: input character
+ * Description: Checks for the uppercase character
+ * Return: 1 if uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	int num;
+
+	char C;
+
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+		if (c == C)
+			num = 1;
+		else
+			num = 0;
+	}
+	return (num);
+}
