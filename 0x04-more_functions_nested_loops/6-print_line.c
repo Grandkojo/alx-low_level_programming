fb4aecce11ad789795b3ff21629842617381803b
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 16:39:12 -0800

    C nested

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * print_line - Check description
+ * @n: input character
+ * Description: Drawsa straight line in the terminal
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	char line = '_';
+
+	if (n == 0)
+		_putchar(line);
+	else
+		_putchar(line * (n + '0'));
+}
