b18531ddca47f3f4a5b7f960d91284b93765fc48
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 18:10:23 -0800

    C nested

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - Fizz Buzz program
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0)
+			printf("Fizz ");
+
+		else if (number % 5 == 0)
+			printf("Buzz ");
+
+		else if ((number % 3 == 0) && (number % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", number);
+	}
+	return (0);
+}
