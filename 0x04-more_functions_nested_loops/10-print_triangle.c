2dcb54b25d12948528b3c609153d202b0aaa3343
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 18:46:09 -0800

    C nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,18 @@
  */
 void print_triangle(int size)
 {
-	int i, j, n = size - 1;
+	int i = 0, j, n = size - 1;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
 				if (j < n)
 					_putchar(' ');
+				else
+					_putchar('#');
 			}
 			n--;
 			_putchar('\n');
