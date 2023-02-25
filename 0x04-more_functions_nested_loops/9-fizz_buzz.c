4b86cfbe4fada91f1ed2f5f82e698ec0b52904a2
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-02-24 18:32:07 -0800

    C nested

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		if (number % 3 == 0 && number % 5 == 0)
 			printf("FizzBuzz ");
 
-		else if (number % 5 == 0)
-			printf("Buzz ");
-
 		else if (number % 3 == 0)
 			printf("Fizz ");
+
+		else if (number % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", number);
 	}
