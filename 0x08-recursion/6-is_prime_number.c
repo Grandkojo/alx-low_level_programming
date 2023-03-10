b70e6469828e2c723d5ada15466dc63a9b95361b
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-09 16:51:27 -0800

    C recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,15 @@
  */
 int is_prime_number(int n)
 {
-	if ((n / 1 == n) && (n % n == 0))
+	if (n < 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (0);
+	}
+	else if ((n / 1 == n) && (n % n == 0))
 	{
 		return (1);
 	}
