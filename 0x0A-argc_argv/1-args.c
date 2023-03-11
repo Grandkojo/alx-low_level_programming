cfa09a83ca9ff74887bb6c2a364ce2a297c3f4ec
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-11 08:42:58 -0800

    C arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,6 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	int count;
+
+	if (argc > 1)
+	{
+	printf("%d\n", argc - 1);
+	}
+	else if (argc == 1)
+		printf("%d\n", 0);
 	return (0);
 }
