85a14c1a41cc5410ea7760728347262a0819f359
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-11 08:25:44 -0800

    C arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+/**
+ * main - Prints the number of arguments paseed into it
+ * @argc: argument integer
+ * @argv: argument array of strings
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
