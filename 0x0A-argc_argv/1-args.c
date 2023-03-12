84c3a10e52d4badb4760103af3850a0b2ef184dd
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-11 18:40:31 -0800

    C arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,5 @@
 # include <stdio.h>
 # include "main.h"
-
 /**
  * main - Prints the number of arguments paseed into it
  * @argc: argument integer
@@ -9,13 +8,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-
-	if (argc > 1)
+	if (argv[0])
 	{
-	printf("%d\n", argc - 1);
+		printf("%d\n", argc - 1);
 	}
-	else
-		printf("%d\n", 0);
 	return (0);
 }
