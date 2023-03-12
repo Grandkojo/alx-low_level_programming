7fff8cd575af698ea1a9fededf34320f054c89b2
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-11 17:34:00 -0800

    C arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - prints the products of two integers but returns error
+ * if the arguments are less thsn 2
+ * @argc: input character
+ * @argv: input array of strings
+ * Return: Always (0)  Success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int mul = 1;
+
+	if (argc >= 2)
+	{
+		for (count = 1; count <= argc - 1; count++)
+		{
+			mul *= atoi(argv[count]);
+		}
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+		printf("Error\n");
+
+	return (1);
+}
