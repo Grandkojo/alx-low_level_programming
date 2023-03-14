91202379fcba1d3096f46afdee10aeee751087af
Grandkojo <essienernest.kojoowusu@mail.com>
2023-03-14 11:04:04 -0700

    C malloc and free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,18 +17,23 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
+	{
 		return (NULL);
+		free(array);
+	}
+
 	for (; i < height; i++)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
 			return (NULL);
+			free(array[i]);
+		}
 	}
 
 	for (i = 0; i < height; i++)
 		for (; j < width; j++)
 			array[i][j] = 0;
 	return (array);
-	free(array);
-	free(array[i]);
 }
