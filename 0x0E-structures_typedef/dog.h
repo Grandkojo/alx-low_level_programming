ef06ef0cb22c9c2ab98af023558c01f51b172fa3
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-03-21 14:25:19 -0700

    C structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
+/**
+ * struct dog - Create a structure for dog
+ * @name: input stringg for dog's name
+ * @age: input integer for dog's age
+ * @owner: owner of dog's name
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
