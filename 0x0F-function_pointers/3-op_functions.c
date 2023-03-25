458fbbc9d7e48d9dae08dd99152473a53c00a698
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-03-24 18:55:55 -0700

    Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * op_add - function that returns the sum of a and b
+ * @a: input intger
+ * @b: input integer
+ * Return: The result of a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a fumction that returns the difference of a and b
+ * @a: an input integer
+ * @b: input integer
+ * Return: The result of a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that returns the product of a and b
+ * @a: input integer
+ * @b: input integer
+ * Return: The result of a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - a funtion that returns the division of a and b
+ * @a: input integer
+ * @b: input integer
+ * Return: The result of a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a funtion that returns the modulo of a and b
+ * @a: input integer
+ * @b: input integer
+ * Return: The result of a % b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
