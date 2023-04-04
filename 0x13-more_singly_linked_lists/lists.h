46098bc11d0bc5aed1957aa3d386472429dd3586
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-04-04 12:21:55 -0700

    More linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _MORE_LINKED_LISTS
 #define _MORE_LINKED_LISTS
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
