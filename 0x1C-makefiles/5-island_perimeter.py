91c3916fba26d14eb6e1b041a1bc6a99096020f7
Grandkojo <essienernest.kojoowusu@gmail.com>
2023-10-25 20:41:47 +0000

    Island perimter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is the perimeter island problem"""
+
+
+def island_perimeter(grid):
+    """This function is used to find the perimeter of an island"""
+
+    if grid is None:
+        return
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+    return perimeter
